f38e026e7b228416479f7c3d1924ed81c574cbd8
Pragya <pragyalachhwani07@gmail.com>
2026-01-28 08:36:28 +0530

    feat: add Flex component and fix App.jsx imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-  import { useState } from 'react'
-  import reactLogo from './assets/react.svg'
-  import viteLogo from '/vite.svg'
-  import './App.css'
-  import Transitions from './components/transitions';
+import { useState } from 'react'
+import './App.css'
+import Transitions from './guide/transition/Transitions';
 
   function App() {
     const [expanded, setExpanded] = useState(true);
